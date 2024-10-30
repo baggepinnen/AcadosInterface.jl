e8e8ae774350b7c42d2296dbc1e285296e05eeb9
Fredrik Bagge Carlson <baggepinnen@gmail.com>
2024-10-30 12:20:07 +0100

    initial commit

--- src/AcadosInterface.jl
+++ src/AcadosInterface.jl
@@ -1,5 +1,205 @@
 module AcadosInterface
+using Symbolics, PyCall
+using LinearAlgebra, Random
 
-# Write your package code here.
+@enum AcadosStatus SUCCESS NAN_DETECTED MAXITER MINSTEP QP_FAILURE READY UNBOUNDED
+
+const casadi = PyCall.PyNULL()
+const np = PyCall.PyNULL()
+
+function __init__()
+    copy!(casadi, pyimport("casadi"))
+    copy!(np, pyimport("numpy"))
+end
+
+for ff in [sin, cos, exp, sqrt]
+    f = nameof(ff)
+    m = Base.parentmodule(ff)
+    eval(quote
+        function ($m.$f)(p::$PyObject)
+            return casadi.$f(p)
+        end
+    end)
+end
+
+function dynamics2casadi(dynamics, nx, nu, p)
+    x = Symbolics.variables(:X, 1:nx)
+    u = Symbolics.variables(:U, 1:nu)
+    vars = [x; u]
+    if hasmethod(dynamics, (typeof(x), typeof(u), typeof(p), Real))
+        dx = dynamics(x, u, p, 0.0)
+    elseif hasmethod(dynamics, (typeof(x), typeof(x), typeof(u), typeof(p), Real))
+        dx = similar(x)
+        dynamics(dx, x, u, p, 0.0)
+    else
+        throw(ArgumentError("dynamics must be a function of the form dynamics(x, u, p, t) or dynamics(dx, x, u, p, t)"))
+    end
+
+    xd_cas = [casadi.SX.sym(string(v)*"_dot") for v in x]
+    x_cas = [casadi.SX.sym(string(v)) for v in x]
+    u_cas = [casadi.SX.sym(string(v)) for v in u]
+
+    vars_cas = [x_cas; u_cas]
+    subs = Dict(vars .=> vars_cas)
+    dx_cas = [Symbolics.substitute(dxi, subs) for dxi in dx]
+
+    XD_cas = casadi.vertcat(xd_cas...)
+    X_cas = casadi.vertcat(x_cas...)
+    U_cas = casadi.vertcat(u_cas...)
+    DX_cas = casadi.vertcat(dx_cas...)
+
+    XD_cas, X_cas, U_cas, DX_cas
+end
+
+struct OCP2
+    model
+    ocp
+    ocp_solver
+    nx
+    nu
+    N
+end
+
+function generate(dynamics;
+    nx,
+    nu,
+    N::Int,
+    Tf,
+    p = nothing,
+    modelname = "model_$(randstring('a':'z', 6))", # Without the random string, changes to settings will have no effect unless julia is restarted
+    x_labels = nothing,
+    u_labels = nothing,
+    Q1 = nothing,
+    Q2 = nothing,
+    QN = nothing,
+    xr = zeros(nx),
+    ur = zeros(nu),
+    xrN = xr,
+    umin = nothing,
+    umax = nothing,
+    xmin = nothing,
+    xmax = nothing,
+    xNmin = nothing,
+    xNmax = nothing,
+    x0 = nothing,
+    qp_solver = "PARTIAL_CONDENSING_HPIPM",
+    hessian_approx = "GAUSS_NEWTON",
+    integrator_type = "ERK",
+    print_level = 0,
+    nlp_solver_type = "SQP",
+    globalization = "MERIT_BACKTRACKING",
+    nlp_solver_max_iter = 100,
+    generate_ocp = true,
+    generate_solver = true,
+    verbose = false,
+)
+
+    XD_cas, X_cas, U_cas, DX_cas = dynamics2casadi(dynamics, nx, nu, p)
+    
+    verbose && @info "Creating model"
+    AcadosModel = pyimport("acados_template").AcadosModel
+    model = AcadosModel()
+    model.f_expl_expr = DX_cas
+    model.f_impl_expr = XD_cas - DX_cas
+    model.xdot = XD_cas
+    model.x = X_cas
+    model.u = U_cas
+    model.name = modelname 
+    x_labels === nothing || (model.x_labels = x_labels)
+    u_labels === nothing || (model.u_labels = u_labels)
+
+    generate_ocp || return model
+
+    verbose && @info "Creating OCP"
+    AcadosOcp = pyimport("acados_template").AcadosOcp
+    ocp = AcadosOcp()
+    ocp.model = model
+    ocp.solver_options.N_horizon = N
+    ocp.solver_options.tf = Tf
+
+
+    # path cost
+    if Q1 !== nothing
+        verbose && @info "Adding path cost"
+        isnothing(Q2) && throw(ArgumentError("Q2 must be provided if Q1 is provided"))
+        ocp.cost.cost_type = "NONLINEAR_LS"
+        ocp.model.cost_y_expr = casadi.vertcat(model.x, model.u)
+        ocp.cost.yref = np.array([xr; ur])
+        ocp.cost.W = casadi.diagcat(np.matrix(Q1), np.matrix(Q2)).full()
+    end
+
+    # terminal cost
+    if QN !== nothing
+        verbose && @info "Adding terminal cost"
+        ocp.cost.cost_type_e = "NONLINEAR_LS"
+        ocp.cost.yref_e = np.array(xrN)
+        ocp.model.cost_y_expr_e = model.x
+        ocp.cost.W_e = np.matrix(QN)
+    end
+
+    # set constraints
+    if umin !== nothing
+        verbose && @info "Adding control constraints"
+        isnothing(umax) && throw(ArgumentError("umax must be provided if umin is provided"))
+        ocp.constraints.lbu = np.array(umin)
+        ocp.constraints.ubu = np.array(umax)
+        ocp.constraints.idxbu = findall(isfinite.(umax) .| isfinite.(umin)) .- 1 # u indices that have bounds declared
+    end
+    
+    if x0 !== nothing
+        verbose && @info "Adding initial state constraints"
+        ocp.constraints.x0 = np.array(x0)
+    end
+
+    # terminal constraints
+    if xNmin !== nothing
+        verbose && @info "Adding terminal state constraints"
+        isnothing(xNmax) && throw(ArgumentError("xNmax must be provided if xNmin is provided"))
+        ocp.constraints.lbx_e = np.array(xNmin)
+        ocp.constraints.ubx_e = np.array(xNmax)
+
+        ocp.constraints.idxbx_e = findall(isfinite.(xNmax) .| isfinite.(xNmin)) .- 1 # x indices that have terminal bounds declared
+    end
+
+
+    # set options
+    verbose && @info "Setting solver options"
+    ocp.solver_options.qp_solver = qp_solver # FULL_CONDENSING_QPOASES
+    # PARTIAL_CONDENSING_HPIPM, FULL_CONDENSING_QPOASES, FULL_CONDENSING_HPIPM,
+    # PARTIAL_CONDENSING_QPDUNES, PARTIAL_CONDENSING_OSQP, FULL_CONDENSING_DAQP
+    ocp.solver_options.hessian_approx = hessian_approx # "GAUSS_NEWTON", "EXACT"
+    ocp.solver_options.integrator_type = integrator_type # IRK, ERK
+    ocp.solver_options.print_level = print_level
+    ocp.solver_options.nlp_solver_type = nlp_solver_type # SQP_RTI, SQP
+    ocp.solver_options.nlp_solver_max_iter = nlp_solver_max_iter
+    ocp.solver_options.globalization = globalization # turns on globalization
+
+    # rm("acados_ocp.json", force=true)
+    # rm("c_generated_code", recursive=true, force=true)
+
+    generate_solver || return (; model, ocp)
+
+    verbose && @info "Generating solver"
+    AcadosOcpSolver = pyimport("acados_template").AcadosOcpSolver
+    ocp_solver = AcadosOcpSolver(ocp)
+
+    OCP2(model, ocp, ocp_solver, nx, nu, N)
+end
+
+function simulate(prob::OCP2, verbose=true)
+    ocp_solver = prob.ocp_solver
+    @time status = ocp_solver.solve() |> AcadosStatus
+    verbose && ocp_solver.print_statistics()
+    status == SUCCESS || @warn "Solver failed: status code $status"
+    (; nx, nu, N) = prob
+    X = zeros(nx, N+1)
+    U = zeros(nu, N)
+    for i = 1:N
+        X[:, i] = ocp_solver.get(i-1, "x")
+        U[:, i] = ocp_solver.get(i-1, "u")
+    end
+    X[:, N+1] = ocp_solver.get(N, "x")
+    X, U
+end
 
 end
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,73 @@
+ENV["PYTHON"] = "/home/fredrikb/repos/acados/bin/python3" # Path to venv python 
+ENV["PYCALL_JL_RUNTIME_PYTHON"] = "/home/fredrikb/repos/acados/bin/python3"
+
 using AcadosInterface
-using Test
+using Test, LinearAlgebra
+
 
 @testset "AcadosInterface.jl" begin
-    # Write your tests here.
+
+    function cartpole(x, u, p, _=0)
+        mc, mp, l, g = 1.0, 0.2, 0.5, 9.81
+    
+        q  = x[[1, 2]]
+        qd = x[[3, 4]]
+    
+        s = sin(q[2])
+        c = cos(q[2])
+    
+        H = [mc+mp mp*l*c; mp*l*c mp*l^2]
+        C = [0 -mp*qd[2]*l*s; 0 0]
+        G = [0, mp * g * l * s]
+        B = [1, 0]
+    
+        qdd = -H \ (C * qd + G - B * u[1])
+        return [qd; qdd]
+    end
+    
+    nu = 1           # number of controls
+    nx = 4           # number of states
+    Ts = 0.04        # sample time
+    N = 80           # Optimization horizon (number of time steps)
+    Tf = N * Ts      # End time
+    x0 = zeros(nx)   # Initial state
+    xr = [0, π, 0, 0] # Reference state is given by the pendulum upright (π rad)
+
+    umax = [10]
+    umin = -umax
+
+    Q1 = Diagonal([1, 1, 1, 1])
+    Q2 = Diagonal([0.01])
+    QN = Q1
+
+
+
+    prob = AcadosInterface.generate(cartpole;
+        nx,
+        nu,
+        N,
+        Tf,
+        x0,
+        xr,
+        umin,
+        umax,
+        Q1,
+        Q2,
+        QN,
+        verbose = true,
+        xNmin = xr,
+        xNmax = xr,
+        nlp_solver_max_iter = 1000,
+    )
+    X, U = AcadosInterface.simulate(prob)
+
+    @test X[:, end] ≈ xr atol=1e-3
+    
+    isinteractive() && plot(
+        plot(X', layout=1),
+        plot(U', layout=nu),
+    )
+    rm("acados_ocp.json", force=true)
+    rm("c_generated_code", recursive=true, force=true)
+
 end
